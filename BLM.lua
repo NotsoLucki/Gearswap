40a275a4659fdfc0cf92ad28b550944c1b4362fb
NotsoLucki <ethodges3@gmail.com>
2016-10-04 16:31:18 -0400

    Update BLM.lua

--- BLM.lua
+++ BLM.lua
@@ -6,18 +6,34 @@
 function get_sets()
 	--Precast occurs before actions are sent to the server
 	sets.precast = {}
-	sets.precast.sid = {main="Eremite's Wand +1", sub="Flat Shield", ammo="Morion Tathlum", head="Seer's Crown +1", neck="Willpower Torque", ear2="Morion Earring", body="Tactician Magician's Coat", hands='Sly Gauntlets', ring1="Wisdom Ring +1", ring2="Wisdom Ring +1", back="Red Cape +1", waist="Druid's Rope", legs="Magic Slacks", feet="Mountain Gaiters"}
+	sets.precast.sid = {
+		main="Eremite's Wand +1", sub="Flat Shield", ammo="Morion Tathlum",
+		head="Seer's Crown +1", neck="Willpower Torque", ear2="Morion Earring",
+		body="Tactician Magician's Coat", hands='Sly Gauntlets', ring1="Wisdom Ring +1", ring2="Wisdom Ring +1",
+		back="Red Cape +1", waist="Druid's Rope", legs="Magic Slacks", feet="Mountain Gaiters"}
 	
 	--Midcast occurs RIGHT/JUST/etc. before actions are sent to the server
 	sets.midcast = {}
-	sets.midcast.ele = {main="Qi Staff +1", sub="Lizard Strap +1", neck="Solon Torque", ear1="Elemental Earring", waist="Reverend Sash"}
-	sets.midcast.enf = {main="Rose Wand +1", sub="Flat Shield", neck="Solon Torque", ear1="Enfeebling Earring", waist="Reverend Sash"}
-	sets.midcast.drk = {main="Rose Wand +1", sub="Flat Shield", neck="Solon Torque", ear1="Dark Earring", waist="Reverend Sash"}
+	sets.midcast.ele = {
+		main="Qi Staff +1", sub="Lizard Strap +1",
+		neck="Solon Torque", ear1="Elemental Earring",
+		waist="Reverend Sash"}
+	sets.midcast.enf = {
+		main="Rose Wand +1", sub="Flat Shield",
+		neck="Solon Torque", ear1="Enfeebling Earring",
+		waist="Reverend Sash"}
+	sets.midcast.drk = {
+		main="Rose Wand +1", sub="Flat Shield",
+		neck="Solon Torque", ear1="Dark Earring",
+		waist="Reverend Sash"}
 	
 	--Aftercast occurs after actions are sent to the server
 	sets.aftercast = {}
 	sets.aftercast.idle = {}
-	sets.aftercast.rest = {main="Spiro Staff", sub="Lizard Strap+1", body="Salutary Robe +1", neck='Beak Necklace +1', ear1='Antivenom Earring', waist="Qiqirn Sash +1", legs="Baron's Slops"}
+	sets.aftercast.rest = {
+		main="Spiro Staff", sub="Lizard Strap+1",
+		body="Salutary Robe +1", neck='Beak Necklace +1', ear1='Antivenom Earring',
+		waist="Qiqirn Sash +1", legs="Baron's Slops"}
 end
 
 -------------------------------------------------------------------------------------------------------------------------------
