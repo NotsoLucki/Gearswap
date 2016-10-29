dc6aecb87cbca1a4ad7720f51476aebedd7e7b6d
NotsoLucki <ethodges3@gmail.com>
2016-10-28 22:03:45 -0400

    Update BLM.lua

--- BLM.lua
+++ BLM.lua
@@ -7,33 +7,42 @@ function get_sets()
 	--Precast occurs before actions are sent to the server
 	sets.precast = {}
 	sets.precast.sid = {
-		main="Eremite's Wand +1", sub="Tortoise Shield", ammo="Morion Tathlum", 
+		main="Eremite's Wand +1", sub="Tortoise Shield", ammo="Phantom Tathlum", 
 		neck="Willpower Torque", ear2="Phantom Earring +1", 
-		body="Demon's Cloak", hands='Sly Gauntlets', ring1="Genius Ring +1", ring2="Genius Ring +1", 
-		back="Red Cape +1", waist="Druid's Rope", legs="Magic Slacks", feet="Wizard's Sabots"}
+		body="Ixion Cloak", hands='Mahatma cuffs', ring1="Omniscient Ring +1", ring2="Omniscient Ring +1", 
+		waist="Druid's Rope", legs="Mahatma slops", feet="Wizard's Sabots"}
 	
 	--Midcast occurs RIGHT/JUST/etc. before actions are sent to the server
 	sets.midcast = {}
 	sets.midcast.ele = {
-		main="Chatoyant Staff", sub="Bugard Strap +1", 
-		neck="Solon Torque", ear1="Elemental Earring", 
-		body="Demon's Cloak", hands="Wizard's Gloves",
-		waist="Penitent's Rope", feet="Mountain Gaiters"}
+		main="Chatoyant Staff", sub="Vivid Strap +1", 
+		head="Demon helm +1", neck="Lemegeton medallion +1", ear1="Moldavite Earring", 
+		body="Genie Weskit", hands="Genie manillas",
+		back="Hecate's Cape", waist="Witch sash", legs="Mahatma slops", feet="Mountain gaiters"}
 	sets.midcast.eleIce = {
 		main="Aquilo's Staff",
-		hands="Wizard's Gloves",}
+		hands="Wizard's Gloves",
+		back="Hecate's Cape"}
 	sets.midcast.enf = {
-		main="Chatoyant Staff", sub="Bugard Strap +1", 
-		head="Wizard's Petasos", neck="Enfeebling Torque", ear1="Enfeebling Earring", 
-		body="Wizard's Coat", hands='Sly Gauntlets', 
-		waist="Penitent's Rope", feet="Mountain Gaiters"}
+		main="Chatoyant Staff", sub="Vivid Strap +1", 
+		head="Genie tiara", neck="Enfeebling Torque", ear1="Enfeebling Earring", 
+		body="Wizard's Coat", hands='Mahatma cuffs', 
+		back="Ixion Cape", waist="Witch Sash", legs="Genie lappas"}
 	sets.midcast.drk = {
-		main="Chatoyant Staff", sub="Bugard Strap +1", 
-		head="Wizard's Petasos", neck="Dark Torque", ear1="Dark Earring", 
-		body="Flora Cotehardie", hands='Sly Gauntlets', 
-		waist="Penitent's Rope", feet="Mountain Gaiters"}
+		main="Chatoyant Staff", sub="Vivid Strap +1", 
+		head="Demon helm +1", neck="Dark Torque", ear1="Dark Earring", 
+		body="Mahatma houppelande", hands='Mahatma cuffs', 
+		back="Ixion Cape", waist="Witch sash", legs="Wizard's Tonban", feet="Genie Huaraches"}
 	sets.midcast.cure = {
-		main="Chatoyant Staff", sub="Bugard Strap +1"}
+		main="Chatoyant Staff", sub="Bugard Strap +1",
+		head="Walahra Turban", neck="Fylgja Torque +1", ear1="Roundel Earring",
+		body="Mahatma houppelande", ring1="Celestial Ring", ring2="Celestial Ring",
+		back="Ixion Cape"}
+	sets.midcast.enh = {
+		main="Chatoyant Staff", sub="Bugard Strap +1",
+		head="Walahra Turban",
+		body="Goliard Saio",
+		back="Ixion Cape", feet="Genie Huaraches"}
 	sets.midcast.SR = {
 		ring1="Sorcerer's Ring"}
 	sets.midcast.UP = {
@@ -43,12 +52,14 @@ function get_sets()
 	sets.aftercast = {}
 	sets.aftercast.idle = {
 		main="Terra's Staff", sub = "Reaver Grip +1",
-		neck="Evasion Torque", ear1="Platinum Earring +1", ear2="Platinum Earring +1",
-		body="Demon's Cloak", hands="Tactician Magician's Cuffs +2", ring1="Celerity Ring +1", ring2="Celerity Ring +1",
-		back="Umbra Cape", waist="Volant Belt", legs="Martial Slacks", feet="Herald's Gaiters"}
+		neck="Orochi nodowa +1", ear1="Harmonius Earring", ear2="Harmonius Earring",
+		body="Ixion Cloak", hands="Creek M mitts", ring1="Harmonius Ring", ring2="Harmonius Ring",
+		back="Umbra Cape", waist="Steppe sash", legs="Genie Lappas", feet="Herald's Gaiters"}
 	sets.aftercast.rest = {
 		main="Chatoyant Staff", sub="Bugard Strap+1", 
-		body="Demon's Cloak", neck='Beak Necklace +1', ear1='Antivenom Earring', 
+		head="Genie tiara",
+		neck='Beak Necklace +1', ear1='Antivenom Earring', ear2="Rapture Earring",
+		body="Mahatma houppelande", hands="Genie Gages", ring1="Celestial Ring", ring2="Celestial Ring",
 		back="Invigorating Cape", waist="Qiqirn Sash +1", legs="Baron's Slops"}
 
 -----------------------------------------------------------------------------------------------------------------------------
@@ -87,6 +98,9 @@ function midcast(spell)
 	end
 	if player.hpp < 51 then
 		equip(sets.midcast.UP)
+	end
+	if spell.skill:contains('Enhancing') then
+		equip(sets.midcast.enh)
 	end
 end
 
